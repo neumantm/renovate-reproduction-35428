ed21202655c74406c999d361461d157d250628d9
Tim Neumann <tim.neumann@vector.com>
2025-12-02 16:43:03 +0100

    chore: Configure as self-hosted

---
+++ renovate-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  repositories: ['neumantm/renovate-reproduction-35428'],
+  extends: [
+    'docker:pinDigests',
+  ],
+  packageRules: [
+    {
+      "matchDatasources": [
+        "docker"
+      ],
+      "pinDigests": false
+    },
+  ],
+};
